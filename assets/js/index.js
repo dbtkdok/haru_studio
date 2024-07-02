86a471dc4a85dec0c251807471259d81ac2746ae
dbtkdok <57377065+dbtkdok@users.noreply.github.com>
2024-07-02 12:01:41 +0900

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,6 @@
 $(function () {
-    $('.content button').click(function(){
-        $('.modal_box').fadeIn(1000);
-        $('.modal_bg').fadeIn(1000);
-    });
 
-    $('.modal_box button').click(function(){
+    $('#modal_close').click(function(){
         $('.modal_box').fadeOut();
         $('.modal_bg').fadeOut();
     });
